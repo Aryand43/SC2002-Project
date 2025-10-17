632e9033a167708120572e524c8cdf81f1742b95
Shailow <low.shayne@yahoo.com.sg>
2025-10-17 20:39:16 +0800

    Main and user class

---
+++ Internship Placement Management System/Main.java
@@ -0,0 +1,7 @@
+
+import models.*;
+public class MainApp {
+    public static void main(String args[]){
+        User user1 = new user("U1234567", "Johnny", "password");
+    }
+}
--- models/User.java
+++ models/User.java
@@ -1,11 +1,66 @@
+import java.util.Scanner;
+
 public class User {
     private int ID;
     private String name;
     private String password;
 
+    /** 
+    * Class constructor.
+    */
     public User(int ID, String name, String pw){
         this.ID = ID;
         this.name = name;
         this.password = pw;
     }
+
+    public int getID(){return this.ID;}
+    public String getUserName(){return this.name;}
+    public String getPassword(){return this.password;}
+
+    public String setUserName(){return this.name;}
+    public boolean setPassword(String oldPass){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            if (oldPass.compareTo(this.password) == 0){
+                System.out.print("\nEnter new password: ");
+                String newPass = sc.nextLine();
+                if (isValidPassword(newPass)) {
+                    this.password = newPass;
+                    break;
+                } 
+                else {
+                    System.out.println("- At least 8 characters");
+                    System.out.println("- At least one uppercase letter");
+                    System.out.println("- At least one digit");
+                    System.out.println("- At least one special character");
+                }
+            }
+        }   
+        sc.close();
+        return true;
+    }
+    
+    public static boolean isValidPassword(String password) {
+        boolean length = password.length() >= 8; // Password length is at least 8
+        boolean upper = password.chars().anyMatch(Character::isUpperCase); // At least 1 upper
+        boolean digit = password.chars().anyMatch(Character::isDigit); // At least 1 number
+        // Regex to check if there is at least 1 special character
+        boolean special = password.matches(".*[!@#$%^&*()_+\\-={};':\"\\\\|,.<>/?].*");
+
+        // ALL conditions met before able to change password
+        if (length && upper && digit && special) {
+            System.out.println("Valid new password");
+            return true;
+        } 
+        
+        else {
+            System.out.println("Invalid new password");
+            return false;
+        }
+    }
+                
+
 }
+
+
