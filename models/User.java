06abcb6093820886f0f16efa47ff53311f8668fb
Shailow <low.shayne@yahoo.com.sg>
2025-10-17 17:40:54 +0800

    Create User.java
    initial start

---
+++ models/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private int ID;
+    private String name;
+    private String password;
+
+    public User(int ID, String name, String pw){
+        this.ID = ID;
+        this.name = name;
+        this.password = pw;
+    }
+}
