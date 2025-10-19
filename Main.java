61989f59a928539c6203b316a3d08e1470d29530
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2025-10-19 20:23:33 +0800

    -

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import models.User;
 import controllers.studentFileHandler;
+import java.util.ArrayList;
 public class Main {
+	private static ArrayList<User> userList = new ArrayList<>();
     public static void main (String args[]){
+    	
         
         // user class testing (not part of project)
-        User user1 = new User("U1234567", "Johnny");
+        User user1 = new User("U1234567", "Johnny","testemai@gmail.com");
         user1.setPassword("randompasstest");
         user1.setPassword("password");
+
         
         studentFileHandler studentfileHandler = new studentFileHandler();
         studentfileHandler.readFromFile();
