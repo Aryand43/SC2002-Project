954230704d803bcec108784ccb28a7bebdad0223
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2025-10-21 02:33:26 +0800

    Update ReportGenerator.java

--- src/controllers/ReportGenerator.java
+++ src/controllers/ReportGenerator.java
@@ -1,5 +1,17 @@
 package controllers;
 
+import java.util.ArrayList;
+
 public class ReportGenerator {
 
+	public void generateReportByStatus(String status) {
+		private ArrayList<InternshipOpportunity> InternshipList = new ArrayList<>();
+		InternshipList = InternshipFileHandler.readFromFile();
+		System.out.println("InternshipID Status Company");
+		for(Internship i: InternshipList) {
+			if(i.getStatus().equals(status)) {
+				System.println(i.getId() + i.getStatus() + i.getCompany());
+			}
+		}
+	}
 }
