e549af91c766aa424ef0b92debdeeceeab93dd2c
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2025-11-16 04:21:43 +0800

    -

--- src/models/User.java
+++ src/models/User.java
@@ -117,6 +117,7 @@ public class User {
     public void resetDefaultPassword() {
     	this.password = "password";
     }
+    
     public String setPassword(String oldPass){
         Scanner sc = new Scanner(System.in);
         while(true){
