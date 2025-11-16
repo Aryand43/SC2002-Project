e4e87af761c31cc3ecb2a88ea0fef9c51c4a0813
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2025-11-16 16:57:35 +0800

    -

--- src/controllers/UserManager.java
+++ src/controllers/UserManager.java
@@ -306,11 +306,11 @@ public class UserManager {
 
     	StudentSerializer studentSerializer = new StudentSerializer();
     	FileHandler<Student> studentFileHandler = new FileHandler<>(studentSerializer);
-    	System.out.println("Writing to Student File");
     	studentFileHandler.writeToFile(this.studentList);
 
     	CompanyRepresentativeSerializer companyRepSerializer = new CompanyRepresentativeSerializer();
     	FileHandler<CompanyRepresentative> companyFileHandler = new FileHandler<>(companyRepSerializer);
     	companyFileHandler.writeToFile(this.repList);
+    	System.out.println("Saving to Files");
     }
 }
